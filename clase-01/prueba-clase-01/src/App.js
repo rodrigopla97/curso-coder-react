ea802d99ccfc917d76ffe09b4c17ab744586c20c
rplaceres <rplaceres@leafnoise.io>
2023-06-22 10:32:07 -0300

    Clase 01 - Inicior React App

---
+++ clase-01/prueba-clase-01/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button onClick={() => console.log("click")}>hola</button>
+    </div>
+  );
+}
+
+export default App;
